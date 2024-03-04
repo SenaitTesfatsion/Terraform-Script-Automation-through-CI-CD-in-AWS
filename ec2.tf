11bc5ed70f94f57fcd11be29da9b65d6a3fbb9fe
Senait Tesfatsion <tesfatsionsenait.h@gmail.com>
2024-03-03 21:06:28 -0500

    update

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region  = "us-east-1"
-  profile = "codebuild-user"
+  profile = "terraform-user"
 }
 
 
@@ -11,17 +11,7 @@ terraform {
     bucket  = "learningapp-terraform-state-bucket"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
-    profile = codebuild-user
-  }
-}
-
-# store the terraform state file in s3
-terraform {
-  backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
-    key     = "build/terraform.tfstate"
-    region  = "us-east-1"
-    profile = "codebuild-user"
+    profile = "terraform-user"
   }
 }
 
